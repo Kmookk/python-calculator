d466a69d1418117fb62fa7bee849bfd4e12806aa
Kmookk <157203627+Kmookk@users.noreply.github.com>
2024-11-24 09:21:12 +0000

    FInal

--- calculator.py
+++ calculator.py
@@ -3,23 +3,48 @@ class Calculator:
         return a + b
 
     def subtract(self, a, b):
-        return b - a
+        if a < 0 and b <0:
+            return a - b
+        
+        if a <0 or b<0:
+            if a<0:
+                return -(abs(a)+b)
+            elif b<0:
+                return (a+abs(b))
+            
+        return a - b
 
     def multiply(self, a, b):
         result = 0
-        for i in range(b+1):
-            result = self.add(result, a)
+        for i in range(abs(b)+1):
+            result = self.add(result, i)
+
+        if a < 0 and b <0:
+            return result
+
+        elif a < 0 or b <0:
+            return -result
+        
         return result
 
     def divide(self, a, b):
         result = 0
-        while a > b:
-            a = self.subtract(a, b)
+        og_a =a
+        while abs(a) >= abs(b):
+            a = self.subtract(abs(a), abs(b))
             result += 1
+
+        if og_a < 0 and b < 0:
+            return result
+        
+        if og_a < 0 or b < 0:
+            return -result
+        
         return result
+
     
     def modulo(self, a, b):
-        while a <= b:
+        while a >= b:
             a = a-b
         return a
 
@@ -28,7 +53,19 @@ if __name__ == "__main__":
     calc = Calculator()
     print("This is a simple calculator class!")
     print("Example: addition: ", calc.add(1, 2))
-    print("Example: subtraction: ", calc.subtract(4, 2))
+    print("Example: subtraction: ", calc.subtract(-2, -2))
+    print("Example: subtraction: ", calc.subtract(-7,2))
+    print("Example: subtraction: ", calc.subtract(7,-2))
+    print("Example: subtraction: ", calc.subtract(7,2))
+    print("Example: subtraction: ", calc.subtract(-7,-2))
+    print("Example: multiplication: ", calc.multiply(-2, -3))
+    print("Example: multiplication: ", calc.multiply(2, -3))
+    print("Example: multiplication: ", calc.multiply(-2, 3))
     print("Example: multiplication: ", calc.multiply(2, 3))
-    print("Example: division: ", calc.divide(10, 2))
+    print("Example: division: ", calc.divide(-10, 2))
+    print("Example: division: ", calc.divide(10, -2))
+    print("Example: division: ", calc.divide(-10, -2))
+    print("Example: division: ", calc.divide(10,2))
+    print("Example: division: ", calc.divide(2,10))
     print("Example: modulo: ", calc.modulo(10, 3))
+    print("Example: modulo: ", calc.modulo(10, 10))
--- test_calculator.py
+++ test_calculator.py
@@ -10,6 +10,70 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(1, 2), 3)
 
     # Add the following test methods to the TestCalculator class:
+    #Add
+    def test_add_1(self):
+        self.assertIsInstance(self.calc.add(1.6,2.5),float)
+
+    def test_add_2(self):
+        self.assertGreater(self.calc.add(1,2),2)
+
+    # #Subtract
+    def test_subtract_1(self):
+        self.assertGreater(self.calc.subtract(4,2),0)
+
+    def test_subtract_2(self):
+        self.assertLess(self.calc.subtract(-4,-2),0)
+
+    def test_subtract_3(self):
+        self.assertEqual(self.calc.subtract(-2,-2),0)
+
+    def test_subtract_4(self):
+        self.assertLess(self.calc.subtract(-2,5),0)
+
+    def test_subtract_5(self):
+        self.assertGreater(self.calc.subtract(7,-2),0)
+
+    def test_subtract_6(self):
+        self.assertLess(self.calc.subtract(-7,2),0)
+
+    # #Multiplication
+    def test_multi_1(self):
+        self.assertEqual(self.calc.multiply(2,3),6)
+
+    def test_multi_2(self):
+        self.assertEqual(self.calc.multiply(2,0),0)
+
+    def test_multi_3(self):
+        self.assertLess(self.calc.multiply(2,-6),0)
+
+    def test_multi_4(self):
+        self.assertGreater(self.calc.multiply(-2,-6),0)
+
+    # #divide 
+    def test_divide_1(self):
+        self.assertEqual(self.calc.divide(10,2),5)
+    
+    def test_divide_2(self):
+        self.assertLess(self.calc.divide(-10,2),0)
+
+    def test_divide_3(self):
+        self.assertLess(self.calc.divide(10,-2),0)
+
+    def test_divide_4(self):
+        self.assertGreater(self.calc.divide(-10,-2),0)
+
+    def test_divide_5(self):
+        self.assertEqual(self.calc.divide(10,10),1)
+    
+    # #modulo 
+    def test_modulo_1(self):
+        self.assertEqual(self.calc.modulo(10,3),1)
+
+    def test_modulo_2(self):
+        self.assertEqual(self.calc.modulo(4,1),0)
+
+    def test_modulo_3(self):
+        self.assertEqual(self.calc.modulo(4,4),0)
 
 if __name__ == '__main__':
     unittest.main()
